31ef65b61bca1cd0bff83f3cfe457a109dd4a54c
Eduardo <e.lpalomo@alumnos.upm.es>
2024-02-02 16:55:26 +0200

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button } from '@mui/material';
 
 const Header = () => {
   return (
-    <header>
-      <h1>My App</h1>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        <Button component={Link} to="/" color="inherit">
+          Home
+        </Button>
+        <Button component={Link} to="/about" color="inherit">
+          About
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
 export default Header;
+
