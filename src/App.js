550c9fca03954d4ed5f89039bc95f30db8a7aecb
Eduardo <e.lpalomo@alumnos.upm.es>
2024-02-02 17:08:02 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+// /src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 import Header from './components/Header';
 import MyContainer from './components/MyContainer';
 import About from './components/About';
+import i18n from './i18n';
 
 const App = () => {
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <Routes>
-          <Route path="/" exact element={<MyContainer/>} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+    <I18nextProvider i18n={i18n}>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<MyContainer/>} />
+            <Route path="/about" element={<About/>} />
+          </Routes>
+        </div>
+      </Router>
+    </I18nextProvider>
   );
 };
 
 export default App;
+
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,30 @@
+// /src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Button component={Link} to="/" color="inherit">
-          Home
+          {t('home')}
         </Button>
         <Button component={Link} to="/about" color="inherit">
-          About
+          {t('about')}
+        </Button>
+        <Button color="inherit" id="fi" onClick={() => changeLanguage('fi')}>
+          Finnish
+        </Button>
+        <Button color="inherit" id="en" onClick={() => changeLanguage('en')}>
+          English
         </Button>
       </Toolbar>
     </AppBar>
@@ -18,4 +32,5 @@ const Header = () => {
 };
 
 export default Header;
+
 
