7c22a78606d6d6eda524c62a6c5991f9fa2a65fd
Eduardo <e.lpalomo@alumnos.upm.es>
2024-02-02 16:20:38 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import MyContainer from './components/MyContainer';
+import About from './components/About';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <MyContainer />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<MyContainer/>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
 
--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,46 +1,13 @@
-import React, { useState } from 'react';
-import MyList from './MyList';
+import React from 'react';
 
 const MyContainer = () => {
-  const [items, setItems] = useState([
-    { id: "1", text: "Item 1", clicked: false },
-    { id: "2", text: "Item 2", clicked: false },
-    { id: "3", text: "Item 3", clicked: false },
-  ]);
-
-  const [newItemText, setNewItemText] = useState("");
-
-  const handleAddItem = () => {
-    const newItem = { id: Date.now().toString(), text: newItemText, clicked: false };
-    setItems(prevItems => [...prevItems, newItem]);
-    setNewItemText("");
-  };
-
-  const handleItemClick = (itemId) => {
-    setItems(prevItems =>
-      prevItems.map(item =>
-        item.id === itemId ? { ...item, clicked: !item.clicked } : item
-      )
-    );
-  };
-
   return (
     <div>
-      <h2>This is MyContainer Component</h2>
-      <MyList header="My List Header" items={items} onItemClick={handleItemClick} />
-
-      <textarea
-        value={newItemText}
-        onChange={(e) => setNewItemText(e.target.value)}
-        placeholder="Enter new item text"
-      />
-
-      <button onClick={handleAddItem}>
-        Add New Item
-      </button>
+      <h2>My Container</h2>
+      <p>Some random text in My Container component.</p>
     </div>
   );
-}
+};
 
 export default MyContainer;
 
